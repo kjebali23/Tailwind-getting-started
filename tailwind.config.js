4c55371ddcbde34f70b469d88fc321d4e9819ba9
kjebali23 <khalil.jebali.kj@gmail.com>
2022-07-17 16:02:18 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/index.html",
+    "./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+  },
+  plugins: [],
+}
